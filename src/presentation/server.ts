9bf7941a2e856a90b44953326651efdcc63ae199
juan <judith1912black@gmail.com>
2023-12-07 16:07:04 -0500

    CRUD RestServer API implementado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,31 +1,39 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface Options {
     port: number;
     publicPath?: string;
+    routes: Router;
 }
-
 
 export class Server {
 
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor( options: Options ){
-        const { port, publicPath = 'public' } = options;
+        const { port, publicPath = 'public', routes } = options;
         this.port = port;
         this.publicPath = publicPath;
+        this.routes = routes;
     }
 
     async start() {
 
         //* Middlewares
+        this.app.use( express.json() ); // raw
+        this.app.use( express.urlencoded({ extended: true }) ); // x-wwww-form-urlencoded
 
         //* Public Folder
         this.app.use( express.static( this.publicPath ) );
 
+        //* Routes
+        this.app.use( this.routes );
+        
+        //* Cualquier ruta no esté definida pasa por acá (ayuda a los SPA)
         this.app.get('*', (req, res) => {
             const indexPath = path.join( `${ __dirname }../../../${ this.publicPath }/index.html` );
             res.sendFile( indexPath );
---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { TodoController } from "./controller";
+
+
+
+export class TodoRoutes {
+
+    static get routes(): Router {
+
+        const router = Router();
+        const todoController = new TodoController();
+
+        router.get('/', ( req, res ) => todoController.getTodos( req, res ) );
+        router.get('/:id', todoController.getTodoById );
+        
+        router.post('/', todoController.createTodo );
+
+        router.put('/:id', todoController.updateTodo );
+
+        router.delete('/:id', todoController.deleteTodo );
+
+
+        return router;
+
+
+    }
+
+
+
+}
